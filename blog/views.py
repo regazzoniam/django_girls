dfe41430aa6590bc4ea9557ab726be644a28fa2b
regazzoniam <amandineregad@gmail.com>
2022-06-04 16:44:21 +0200

    update template blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# création d'une vue "post_list" auquelle blog/urls.py fait référence
+# on créé une fonction (def) appelée post_list qui prend une request et qui va return la valeur donnée par une autre fonction render qui va assembler notre template blog/post_list.html.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
