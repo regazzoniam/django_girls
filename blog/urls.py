da3932cd93b6d8bd7925b89f996f30ff931df59e
regazzoniam <amandineregad@gmail.com>
2022-06-06 15:37:26 +0200

    post details view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # post/ : l’URL doit commencer par "post" suivi d’un /
+    # <int:pk> : Django s’attend à une valeur entière (int), qu'en suite il transférera à une vue comme variable nommée "pk".
+    # / : il nous faut un / à nouveau avant la fin de l’URL.
 ]
